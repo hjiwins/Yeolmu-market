b00fd7f13d0a5c2f6bdd0b20cb5be69c0a035754
Juan Gonzalez <jgonzalez@naver.com>
2022-02-23 11:46:58 +0900

    added redux dispatch to fetch calls

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { login } from '../redux/authSlice'
 import { Link } from "react-router-dom";
 import LoginForm from "../components/authentication/LoginForm";
+import { useEffect } from "react";
+
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const storedUserData = useSelector((state) => state.authSlice.userData);
+  useEffect(() => {
+    storedUserData.isLoggedIn && alert(`Welcome ${storedUserData.username}`);
+  }, [storedUserData])
+
   const sendLoginRequest = (formData) => {
     fetch("http://localhost:3000/api/user/login", {
       method: "POST",
@@ -11,22 +21,29 @@ const Login = () => {
       },
     })
       .then((data) => data.json())
-      .then(result => {
+      .then((result) => {
         if (result.errorMessage) {
-          alert(`${result.errorMessage}`)
+          alert(`${result.errorMessage}`);
         } else {
-          alert(`Welcome ${result.username}`)
+          dispatch(login({
+            isLoggedIn: true,
+            userId: result.userId,
+            username: result.username,
+          }));
         }
       })
       .catch((error) => console.log(error));
   };
+
   return (
-    <section class="authenticationSection">
+    <section className="authenticationSection">
+      {storedUserData.isLoggedIn && <p>User logged as {storedUserData.username} </p>}
       <h1>로그인</h1>
       <LoginForm onSubmit={sendLoginRequest} />
       <p>
         회원가입 안 하셨나요? <Link to="/register">회원가입</Link> 하세요!
       </p>
+      
     </section>
   );
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,18 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import RegistrationForm from "../components/authentication/RegistrationForm";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../redux/authSlice";
 
 const Register = () => {
-  
+  const dispatch = useDispatch();
+  const storedUserData = useSelector(state => state.authSlice.userData);
+  useEffect(() => {
+    storedUserData.isLoggedIn && alert(
+      `The account ${storedUserData.username} has been registered successfully!`
+    );
+  }, [storedUserData])
+
   const sendRegistrationRequest = (formData) => {
     fetch("http://localhost:3000/api/user/register", {
       method: "POST",
@@ -12,22 +22,29 @@ const Register = () => {
       },
     })
       .then((data) => data.json())
-      .then(result => {
+      .then((result) => {
         if (result.errorMessage) {
-          alert(`${result.errorMessage}`)
+          alert(`${result.errorMessage}`);
         } else {
-          alert(`Your account ${result.email} has been registered successfully!`);
+          return dispatch(register({
+            isLoggedIn: true,
+            userId: result.userId,
+            username: result.username,
+          }));
         }
-        
       })
       .catch((error) => console.log(error));
+
+    
   };
 
   return (
     <section class="authenticationSection">
       <h1>회원가입</h1>
-      <RegistrationForm onSubmit={sendRegistrationRequest}/>
-      <p>이미 회원가입 하셨나요? <Link to="/login">로그인하세요!</Link></p>
+      <RegistrationForm onSubmit={sendRegistrationRequest} />
+      <p>
+        이미 회원가입 하셨나요? <Link to="/login">로그인하세요!</Link>
+      </p>
     </section>
   );
 };
