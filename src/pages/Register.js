78526121eeb85d2ed74f343d17386ba914c903ae
Juan Gonzalez <jgonzalez@naver.com>
2022-03-03 14:11:54 +0900

    modified the register component and functions to account for both firebase storage and backend api

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,47 +1,83 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useState } from "react";
 import RegistrationForm from "../components/authentication/RegistrationForm";
-import { useDispatch, useSelector } from "react-redux";
-import { register } from "../redux/authSlice";
+import { storage } from "../firebase";
+import { ref } from "@firebase/storage";
+import { getDownloadURL, uploadBytesResumable, deleteObject } from "firebase/storage";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Register = () => {
-  const dispatch = useDispatch();
-  const storedUserData = useSelector(state => state.authSlice.userData);
-  useEffect(() => {
-    storedUserData.isLoggedIn && alert(
-      `The account ${storedUserData.username} has been registered successfully!`
-    );
-  }, [storedUserData])
+  // const dispatch = useDispatch();
+  const history = useHistory();
+  const [uploadProgress, setUploadProgress] = useState(0);
 
-  const sendRegistrationRequest = (formData) => {
-    fetch("http://localhost:3000/api/user/register", {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
+  const sendRegistrationRequest = async (formData) => {
+    // If no image attach then cancel
+    if (!formData.image) return;
+
+    // Start image upload to firebase
+    const storageRef = ref(storage, `/avatars/${formData.image.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, formData.image);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        console.log("current progress", progress);
+        setUploadProgress(progress);
       },
-    })
-      .then((data) => data.json())
-      .then((result) => {
-        if (result.errorMessage) {
-          alert(`${result.errorMessage}`);
-        } else {
-          return dispatch(register({
-            isLoggedIn: true,
-            userId: result.userId,
-            username: result.username,
-          }));
-        }
-      })
-      .catch((error) => console.log(error));
-
-    
+      (error) => {
+        // if error during upload
+        console.log("Error happened during the avatar upload");
+        throw Error(error);
+      },
+      () => {
+        // if avatar upload is successful
+        // Get avatar url
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log("Url Received: ", url);
+          // Start POST request
+          fetch("http://localhost:3000/api/user/register", {
+            method: "POST",
+            body: JSON.stringify({ ...formData, image: url }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((data) => data.json())
+            .then((result) => {
+              console.log(result);
+              if (result.errorMessage) {
+                // if the form submit fails for any reason, then delete the uploaded avatar
+                const avatarToDelete = ref(storage, uploadTask.snapshot.ref._location.path_);
+                deleteObject(avatarToDelete)
+                .then(() => {
+                  console.log('avatar deleted successfully');
+                  alert(`${result.errorMessage}`);
+                })
+                .catch(error => console.log(error));
+              } else {
+                // If everything is successfull, redirect to home.
+                  alert(`User ${result.username} created successfully`);
+                  history.push('/');
+              }
+            })
+            .catch((error) => console.log(error));
+          return;
+        });
+      }
+    );
   };
 
   return (
     <section class="authenticationSection">
       <h1>회원가입</h1>
-      <RegistrationForm onSubmit={sendRegistrationRequest} />
+      <RegistrationForm
+        onSubmit={sendRegistrationRequest}
+        uploadProgress={uploadProgress}
+      />
       <p>
         이미 회원가입 하셨나요? <Link to="/login">로그인하세요!</Link>
       </p>
