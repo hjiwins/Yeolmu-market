83349b22f324a3facae311d778c15b1a8d5b58c8
Juan Gonzalez <jgonzalez@naver.com>
2022-03-03 14:38:27 +0900

    added logout button and minor tweaks

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,24 +1,51 @@
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import styles from './Header.module.css';
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import styles from "./Header.module.css";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/authSlice";
 
 function Header() {
-  const storedUserData = useSelector(state => state.authSlice.userData);
+  const dispatch = useDispatch();
+
+  const storedUserData = useSelector((state) => state.authSlice.userData);
   return (
     <header className={styles.header}>
       <h1>
-        <Link to='/'>열무 마켓</Link>
+        <Link to="/">열무 마켓</Link>
       </h1>
       <nav>
         <ul>
           <li>
-            <Link to='/new-item'>물건올리기</Link>
+            <Link to="/new-item">물건올리기</Link>
           </li>
           <li>
-            <Link to='/favorites'>위시리스트</Link>
+            <Link to="/favorites">위시리스트</Link>
           </li>
-          {storedUserData.isLoggedIn && <li><img className={styles.userAvatar} height="75px" width="75px" alt='user avatar' src={storedUserData.avatar}></img></li>}
-          {storedUserData.isLoggedIn ? <li>{storedUserData.username}으로 로그인됨</li> : <li><Link to='/login'>로그인</Link></li>}
+          {storedUserData.isLoggedIn && (
+            <li>
+              <img
+                className={styles.userAvatar}
+                height="75px"
+                width="75px"
+                alt="user avatar"
+                src={storedUserData.avatar}
+              ></img>
+            </li>
+          )}
+          {storedUserData.isLoggedIn ? (
+            <li>{storedUserData.username}님</li>
+          ) : (
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+          )}
+          {storedUserData.isLoggedIn && (
+            <li onClick={() => dispatch(logout())}>
+              <Link to='/'>
+                로그아웃
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
