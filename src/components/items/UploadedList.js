8e3c56ea538ee3cc2134854b7cdc399e8e7cea45
Juan Gonzalez <jgonzalez@naver.com>
2022-03-24 18:07:48 +0900

    changed prop name to adjust to the new API + TODO added

--- src/components/items/UploadedList.js
+++ src/components/items/UploadedList.js
@@ -1,14 +1,16 @@
 import style from './UploadedList.module.css';
 import UploadedItems from "./UploadedItems";
 
+// TODO: Change name to UploadedItem or similar because this component represent just ONE item
+
 function UploadedList(props) {
   return (
     <ul className={style.list}>
       {props.items.map((item) => (
         <UploadedItems
-          key={item.id}
-          id={item.id}
-          image={item.image}
+          key={item._id}
+          id={item._id}
+          photo={item.photo}
           title={item.title}
           price={item.price}
           seller={item.seller}
