c66450ffb5400da0c249395ca27057e4d3fb9e58
Juan Gonzalez <jgonzalez@naver.com>
2022-04-13 12:58:04 +0900

    refactored photo upload and added progress %

--- src/components/items/NewItemForm.js
+++ src/components/items/NewItemForm.js
@@ -5,8 +5,6 @@ import styles from "./NewItemForm.module.css";
 function NewItemForm(props) {
   const titleInputRef = useRef();
   const priceInputRef = useRef();
-  const photoInputRef = useRef();
-  const userNameInputRef = useRef();
   const detailInputRef = useRef();
 
   function submitHandler(event) {
@@ -14,21 +12,17 @@ function NewItemForm(props) {
 
     const inputTitle = titleInputRef.current.value;
     const inputPrice = priceInputRef.current.value;
-    const inputPhoto = photoInputRef.current.target.files[0];
-    const inputUserName = userNameInputRef.current.value;
+    const inputPhoto = event.target[2].files[0];
     const inputDetail = detailInputRef.current.value;
-
     const itemData = {
       title: inputTitle,
       price: inputPrice,
       photo: inputPhoto,
-      userName: inputUserName,
-      detail: inputDetail,
+      details: inputDetail,
     };
 
     props.onAddItem(itemData);
   }
-
 
   return (
     <form className={styles.form} onSubmit={submitHandler}>
@@ -45,11 +39,14 @@ function NewItemForm(props) {
       />
 
       <label htmlFor="photo">사진</label>
-      <input id="photo" class={styles.photoUploader} type="file" accept="image/*" />
+      <input
+        id="photo"
+        class={styles.photoUploader}
+        type="file"
+        accept="image/*"
+      />
+      {props.uploadProgress !== 0 && <p>Upload {props.uploadProgress} % completed.</p>}
       <button className={styles.photoUploadBtn}>사진 업로드</button>
-
-      <label htmlFor="user-name">작성자</label>
-      <input required id="user-name" type="text" ref={userNameInputRef} />
 
       <label htmlFor="detail">내용</label>
       <textarea required id="detail" rows="10" cols="30" ref={detailInputRef} />
