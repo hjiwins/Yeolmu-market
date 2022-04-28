3ede69c954d9984d4ddfaf8ddf777eaebf85e70a
Juan Gonzalez <jgonzalez@naver.com>
2022-04-28 18:48:55 +0900

    added autologin login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import AllItems from "./pages/AllItems";
@@ -8,7 +8,51 @@ import Layout from "./components/layout/Layout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+import { useDispatch } from "react-redux";
+import { login } from "./redux/authSlice"
+
 function App() {
+  const token = document.cookie.split("=")[1];
+  const autologin = () => {
+    // Sends a request to the API to verify the existent cookie with the token.
+    //
+    // If the token is correct, dispatch the login reducer.
+    // If the token is incorrect, do nothing.
+    fetch("http://localhost:3000/api/user/autologin", {
+      method: "POST",
+      body: JSON.stringify({ token: token }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then((result) => {
+        if (result.errorMessage) {
+          alert(`${result.errorMessage}`);
+        } else {
+          // Save the token in a cookie
+          document.cookie = `token=${result.token}`;
+          // Save login data in Redux
+          dispatch(
+            login({
+              isLoggedIn: true,
+              userId: result.userId,
+              username: result.username,
+              avatar: result.avatar,
+              token: result.token,
+            })
+          );
+        }
+      });
+  };
+  const dispatch = useDispatch();
+  useEffect(() => {
+    //TODO: Check if there is a cookie
+    const token = document.cookie.split("=")[1];
+    // TODO: if the cookie is not there don't do anything
+    // TODO: If the cookie is present, execute the autologin function
+    autologin()
+  }, []);
   return (
     <Layout>
       <Switch>
